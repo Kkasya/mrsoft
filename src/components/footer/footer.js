122d5b51af321cfae2f320d1ab6d37fed35d13bd
Anastassia Kozlovskaya <kasya--k@yandex.ru>
2021-05-18 09:15:41 +0300

    feat: add build

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -7,7 +7,7 @@ const Footer = () => {
 			<div className='footer__developer' >
 				<span >Anastassia Kozlovskaya</span >
 				<span >
-					<a target='_blank' href="https://github.com/Kkasya/" >
+					<a target='_blank' rel="noreferrer"  href="https://github.com/Kkasya/" >
        			 <img className="footer__developer-icon"
 									src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/91/Octicons-mark-github.svg/1200px-Octicons-mark-github.svg.png"
 									alt="github" />
