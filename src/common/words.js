d8078c2965fcb039320549b4546aef441d20d74c
Anastassia Kozlovskaya <kasya--k@yandex.ru>
2021-05-18 09:03:57 +0300

    feat: add adaptive

--- src/common/words.js
+++ src/common/words.js
@@ -1,11 +1,9 @@
-import {data} from "./data.json";
-
 const getData = async () => {
 	const proxyUrl = 'https://cors-anywhere.herokuapp.com/';
 	const targetUrl = 'http://www.mrsoft.by/data.json';
 	const res = await fetch(proxyUrl + targetUrl);
 	if (!res.ok) {
-		return data;
+		throw new Error('data not available');
 	}
 
 	return await res.json();
