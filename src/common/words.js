697aa2b52ef3bfa3502bbf0866a23044983976cc
Anastassia Kozlovskaya <52949341+Kkasya@users.noreply.github.com>
2021-05-18 22:53:16 +0300

    fix: set url to netlify

--- src/common/words.js
+++ src/common/words.js
@@ -1,5 +1,5 @@
 const getData = async () => {
-	const targetUrl = 'http://www.mrsoft.by/data.json';
+	const targetUrl = 'https://kkasya-mrsoft.netlify.app/mrsoft.by/data.json';
 	const res = await fetch(targetUrl);
 	if (!res.ok) {
 		throw new Error('data not available');
